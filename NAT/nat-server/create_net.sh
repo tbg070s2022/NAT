daf386daa79ec70b18f0fe6a09b9bd0a62c2b405
uralsarp <uralsarp@sabanciuniv.edu>
2022-08-17 11:43:58 +0300

    Merge branch 'main' of https://github.com/tbg070s2022/NAT into main

--- NAT/nat-server/create_net.sh
+++ NAT/nat-server/create_net.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 
 # --interactive denedik olmadi gibi
-sudo docker network create ntw1
-sudo docker network create ntw2
+# sudo docker network create ntw1
+# sudo docker network create ntw2
 
 # sudo docker network  create -d ipvlan --internal ntw1
 # sudo docker network  create -d ipvlan  ntw2
 
+# sudo docker network  create -d ipvlan  ntw1
+# sudo docker network  create -d ipvlan --internal ntw2
+
 # sudo docker network create -d macvlan --internal ntw1
 # sudo docker network create -d macvlan ntw2
+
+# sudo docker network create -d macvlan ntw1
+# sudo docker network create -d macvlan --internal ntw2
+
+# sudo docker network create -d macvlan ntw1
+# sudo docker network create -d macvlan ntw2
+
+# sudo docker network create ntw1
+# sudo docker network create --internal ntw2
+
+sudo docker network create --internal ntw1
+sudo docker network create ntw2
+
 
--- NAT/nat-server/start.sh
+++ NAT/nat-server/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 route del default
 ifconfig eth0 down
@@ -6,12 +6,10 @@ ifconfig eth0 down
 iptables -F
 iptables -t nat -F
 iptables -t nat --delete-chain
- 
-iptables -t nat -A POSTROUTING --out-interface eth2 -j MASQUERADE
-iptables -t nat -A POSTROUTING --out-interface eth1 -j MASQUERADE
-iptables -A FORWARD --in-interface eth1 -j ACCEPT
 
- 
+iptables -t nat -A POSTROUTING --out-interface eth1 -j MASQUERADE
+iptables -A FORWARD --in-interface eth2 -j ACCEPT
+
 # service iptables save
 # service iptables restart
 #  
